fb8a993498ad935145d9043f36351abb523ce345
2Kelvin <rocketmancodes02@gmail.com>
2024-07-18 13:03:19 +0300

    done implementing adaptive layout design

--- app/src/main/java/com/example/reply/ui/ReplyApp.kt
+++ app/src/main/java/com/example/reply/ui/ReplyApp.kt
@@ -1,35 +1,43 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.reply.ui
 
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.reply.data.Email
 import com.example.reply.data.MailboxType
+import com.example.reply.ui.utils.ReplyNavigationType
 
 @Composable
 fun ReplyApp(
+    windowSize: WindowWidthSizeClass, // tracking the device's width not height to change layout
     modifier: Modifier = Modifier,
 ) {
     val viewModel: ReplyViewModel = viewModel()
     val replyUiState = viewModel.uiState.collectAsState().value
 
+    // navigation type from enum class
+    val navigationType: ReplyNavigationType
+
+    // changing app layout based on the device size (width)
+    when(windowSize) {
+        WindowWidthSizeClass.Compact -> { // below 600 'px' (phones)
+            navigationType = ReplyNavigationType.BOTTOM_NAVIGATION
+        }
+        WindowWidthSizeClass.Medium -> {  // between 600 and 899 'px' (tablets)
+            navigationType = ReplyNavigationType.NAVIGATION_RAIL
+        }
+        WindowWidthSizeClass.Expanded -> { // above 900 'px' (large screens)
+            navigationType = ReplyNavigationType.PERMANENT_NAVIGATION_DRAWER
+        }
+        else -> { // default layout is the bottom navigation
+            navigationType = ReplyNavigationType.BOTTOM_NAVIGATION
+        }
+    }
+
     ReplyHomeScreen(
+        navigationType = navigationType,
         replyUiState = replyUiState,
         onTabPressed = { mailboxType: MailboxType ->
             viewModel.updateCurrentMailbox(mailboxType = mailboxType)
